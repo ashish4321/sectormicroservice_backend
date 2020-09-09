d8346433174f1f37864a9d8b9a678a10cae6b5f7
unknown <c2wfjava09@iiht.tech>
2020-09-09 14:14:23 +0530

    changed

--- src/test/java/com/wellsfargo/stockmarket/stockdataretrieval/controller/ControllerTest.java
+++ src/test/java/com/wellsfargo/stockmarket/stockdataretrieval/controller/ControllerTest.java
@@ -37,26 +37,6 @@ public class ControllerTest {
 	@MockBean
 	private SectorService sectorService;
 	
-	
-	@Test
-	@DisplayName("Test all sectors found failure scenario - GET /sectors")
-	public void testAllSectorsFoundFailure() throws Exception{
-		Sector firstSector = new Sector(1L,"first sector","this is first sector");
-		Sector SecondSector = new Sector(2L,"second sector","this is second sector");
-		
-		List<Sector> sectors = new ArrayList<>();
-		sectors.add(firstSector);
-		sectors.add(SecondSector);
-		
-		doReturn(sectors).when(sectorService).getAllSectors();
-
-		mockMvc.perform(MockMvcRequestBuilders.get("/sectors"))
-		.andExpect(status().isOk())
-		.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-		.andExpect(jsonPath("$[0].sectorName",is("first")))
-		.andExpect(jsonPath("$[1].sectorName",is("second")));
-	}
-	
 	@Test
 	@DisplayName("Test all sectors found success scenario - GET /sectors")
 	public void testAllSectorsFoundSuccess() throws Exception{
@@ -77,21 +57,6 @@ public class ControllerTest {
 	}
 	
 	@Test
-	@DisplayName("Test Sector Found Failure Scenario - GET /sectors/1")
-	public void testGetSectorByIdFailure() throws Exception{
-		
-		Sector mockSector = new Sector(1L,"mock","this is mock sector");
-		doReturn(mockSector).when(sectorService).getSector(mockSector.getSectorId());
-		
-		mockMvc.perform(MockMvcRequestBuilders.get("/sectors/{sectorid}", 1))
-		.andExpect(status().isOk())
-		.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-		.andExpect(jsonPath("$.sectorId",is(1)))
-		.andExpect(jsonPath("$.sectorName",is("mock")))
-		.andExpect(jsonPath("$.sectorBrief",is("this is a mock sector")));
-	}
-	
-	@Test
 	@DisplayName("Test Sector Found Success Scenario - GET /sectors/1")
 	public void testGetSectorByIdSuccess() throws Exception{
 		
@@ -103,27 +68,7 @@ public class ControllerTest {
 		.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
 		.andExpect(jsonPath("$.sectorId",is(1)))
 		.andExpect(jsonPath("$.sectorName",is("mock")))
-		.andExpect(jsonPath("$.sectorBrief",is("this is mock sector")));
-	}
-	
-	@Test
-	@DisplayName("Test all companies in a sector found failure scenario - GET /sectors/1/companies")
-	public void testCompaniesInSectorFailure() throws Exception{
-		
-		Sector mockSector = new Sector(1L,"mock","this is mock sector");
-		Company mockCompany1 = new Company(1000,"mock company 1", "mock",4001);
-		Company mockCompany2 = new Company(1001,"mock company 2", "mock",4002);
-		List<Company> mockCompanies = new ArrayList<>();
-		mockCompanies.add(mockCompany1);
-		mockCompanies.add(mockCompany2);
-		
-		doReturn(mockCompanies).when(sectorService).getCompany(mockSector.getSectorId());
-		
-		mockMvc.perform(MockMvcRequestBuilders.get("/sectors/{sectorid}/companies", 1))
-		.andExpect(status().isOk())
-		.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-		.andExpect(jsonPath("$[0].companyName",is("this is mock company 1")))
-		.andExpect(jsonPath("$[1].companyName",is("this is mock company 2")));
+		.andExpect(jsonPath("$.brief",is("this is mock sector")));
 	}
 	
 	@Test
@@ -146,23 +91,6 @@ public class ControllerTest {
 		.andExpect(jsonPath("$[1].companyName",is("mock company 2")));
 	}
 
-	@Test
-	@DisplayName("Test delete sector found failure scenario - DELETE /sectors")
-	public void testDeleteSectorFailure() throws Exception{
-		
-		Sector mockSector = new Sector(1L,"mock","this is mock sector");
-		
-		doReturn("sector deleted").when(sectorService).deleteSector(mockSector.getSectorId());
-		
-		String result = mockMvc.perform(MockMvcRequestBuilders.delete("/sectors/{sectorid}", 1))
-						.andReturn()
-						.getResponse()
-						.getContentAsString();
-		
-		Assertions.assertEquals("sector not found", result);
-		
-	}
-	
 	@Test
 	@DisplayName("Test delete sector found success scenario - DELETE /sectors")
 	public void testDeleteSectorSuccess() throws Exception{
